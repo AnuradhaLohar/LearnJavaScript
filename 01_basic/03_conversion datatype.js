9d20724edebcfbc5f12952ddeb951410e21af1e4
AnuradhaLohar <142400130+AnuradhaLohar@users.noreply.github.com>
2024-06-14 08:33:33 +0000

    datatypes conversions and operation

---
+++ 01_basic/03_conversion datatype.js
@@ -0,0 +1,66 @@
+let dim = 1
+
+//console.log(typeof dim);
+//console.log(typeof(dim));       
+
+let valueInnumber = Number(dim) //convert string to number
+//console.log(typeof valueInnumber);//show type of value
+//console.log(valueInnumber);//
+
+let valueInBoolean=Boolean(dim)
+//console.log(typeof valueInBoolean);
+//console.log(valueInBoolean);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let someNumber = 1
+
+let stringNumber = String(someNumber)
+ //
+ //console.log(stringNumber);
+ //console.log(typeof stringNumber);
+
+ //******************************Operation**********************************//
+
+ let value = 3
+ let negvalue = -value
+ //console.log(negvalue);
+
+ // console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = 1
+let str2 = " Radha"
+//console.log(str1 + str2);
+
+
+ //console.log("1" + 2); //op = 12
+//console.log(1 + "2");  //op = 12
+//console.log("1" + 2 + 2);  // op = 122
+// console.log(1 + 2 + "2"); // op = 32 
+ // take a first string value they not adding values but u have pass last sting value they adding first two values// 
+
+ // console.log( (3 + 4) * 5 % 3);
+
+//console.log(+true); //conversion
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+//console.log(num1,num2,num3);
+
+let gameCounter = 10
+++gameCounter;
+++gameCounter;
+++gameCounter;
+++gameCounter;
+console.log(gameCounter);
