7633dfe8a492e0eaae066f7b9828acefccf7b800
AnuradhaLohar <142400130+AnuradhaLohar@users.noreply.github.com>
2024-06-14 06:29:44 +0000

    variables done

---
+++ 01_basic/variable.js
@@ -0,0 +1,7 @@
+const accountId = "12344"
+let accountEmail="Anu@gmail.com"
+var accountpassword="12345"
+accountcity="pune"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountcity])
