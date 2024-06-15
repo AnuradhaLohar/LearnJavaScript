ff91edbe9b15611d8e19494d356cf88fb58a09e6
AnuradhaLohar <142400130+AnuradhaLohar@users.noreply.github.com>
2024-06-15 08:34:08 +0000

    array2

---
+++ 02_basic/02_Array.js
@@ -0,0 +1,32 @@
+const bhand = ["saki","kk","sankya","ajya"]
+const members = ["swala","anju","sam","sam2"]
+const all = ["sorya","saya"]
+
+//bhand.push(members)
+
+//console.log(bhand);
+
+// const bhandmembers = bhand.concat(members)
+// console.log(bhandmembers);
+
+//const bhandmembers = [...bhand,...members,...all]
+ //console.log(bhandmembers);       //combining multiple array
+
+//  const another =[1,2,3,4,5,5,[5,4,8,7,8],2,5,[6,4,[5,6,3,9]]]
+// console.log(another);
+
+// const real = another.flat(Infinity)
+// console.log(real);
+
+//console.log(Array.from("radha"));
+//console.log(Array.isArray("radha"));
+//console.log(Array.from({name:"anu"}));
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+// combine multiple arrays = Array.of 
