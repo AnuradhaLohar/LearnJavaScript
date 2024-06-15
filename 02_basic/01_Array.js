8e0bcb4d93bc7f0cd7c509af556f6488e0fc20c5
AnuradhaLohar <142400130+AnuradhaLohar@users.noreply.github.com>
2024-06-15 07:11:21 +0000

    array done

---
+++ 02_basic/01_Array.js
@@ -0,0 +1,39 @@
+const myarr = [1,2,3,4,5]
+const myArr2 = new Array(1, 2, 3, 4)
+//console.log(myarr);
+//console.log(myArr2);
+
+// ==============================Array Methods========================= 
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.push(8)
+// myarr.pop()
+// console.log(myarr);
+
+// myarr.unshift(8)
+// console.log(myarr);
+
+// myarr.shift()
+// console.log(myarr);
+
+
+// console.log(myarr.includes(2)); 
+// console.log(myarr.indexOf(4));
+
+const newarr = myarr.join()
+
+// console.log(myarr);
+// console.log(newarr);
+
+// slice, splice
+
+console.log("Ajya",myarr);
+
+const myn = myarr.slice(1,3)
+console.log(myn);
+
+const myn2 = myarr.splice(1,3)
+console.log(myn2);   //op = 123
+
+console.log("Anu",myarr);
