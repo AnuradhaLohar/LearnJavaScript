5ef1abd029c0bde5892e7854b432c0010a6f62e2
AnuradhaLohar <142400130+AnuradhaLohar@users.noreply.github.com>
2024-06-18 11:39:31 +0000

    for

---
+++ 05_itration_loops/01_for.js
@@ -0,0 +1,72 @@
+/// for
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+//     
+// }
+
+//*********************************************************************** */
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10 ; j++) {
+        const element = j;
+        
+        if (i==2 || i==5) {
+             
+        // console.log(`${i} * ${j} = ${i*j}`);
+        // console.log(`${i} * ${j} =` +  i*j); //another method
+        //  console.log(i + " * " + j + " =" + i*j); //another method
+        }
+       
+    }
+}
+
+//************************************************************************** */
+
+// for (let i = 0; i <=10; i++) {
+//     const element = i;
+//     if (i==5) {
+//         console.log(`5 is best`);
+        
+//     }
+// }
+
+
+//************************************************************************** */
+
+const arr = ["anu","ajya"]
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    // console.log(element);
+    
+}
+
+
+//**************************************************************************
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (i==7) {
+        // console.log(`loop continue at.....${i}`); //break statement
+        break
+        
+    }
+    // console.log(element);
+    
+}
+
+
+//**************************************************************************
+
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (i==7) {
+        console.log(`loop continue at.....${i}`); // continue statement 
+        continue
+        
+    }
+    console.log(element);
+    
