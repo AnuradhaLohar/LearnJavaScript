99f76936b4cc5585e80dfac1e6803c35ce0f0d1f
AnuradhaLohar <142400130+AnuradhaLohar@users.noreply.github.com>
2024-06-18 12:48:59 +0000

    while_dowhile done

---
+++ 05_itration_loops/02_while_Dowhile.js
@@ -0,0 +1,38 @@
+//while loop
+
+// while (2==2) {
+//     console.log(true);
+//     break
+// }
+
+//****************************************************************************/
+
+// let score = 2
+// while (score <= 10) {
+//     for (let i = 1; i <= 10; i++) {
+//         const element = i;
+//         //console.log(`${score} * ${i} = ${score*i}`);
+        
+//     }
+//     score++
+// }
+
+
+//******************************************************************************/
+
+let arr = ["anu","ajya","kk","saki"]
+let myarray=0
+while (myarray < arr.length) {
+    //console.log(arr[myarray]);
+    myarray++
+}
+
+
+//******************************************************************************/
+
+let score = 11
+
+do {
+    //console.log(score);
+    score++
+} while (score <= 10);
