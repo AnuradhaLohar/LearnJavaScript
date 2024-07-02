57ac44eedd98f5c3a844149208804f4321095307
AnuradhaLohar <loharanuradha644@gmail.com>
2024-07-02 15:33:01 +0530

    static properties

---
+++ 10_classes_and_oop/07_staticprop.js
@@ -0,0 +1,30 @@
+class user{
+   constructor(username){
+    this.username=username
+   }
+
+   logMe(){
+    console.log(`username is ${this.username}`);
+   }
+   createId(){
+    return`1234`
+   }
+
+}
+
+const hitesh= new user("hitesh")
+console.log(hitesh.createId());
+
+
+
+
+class client extends user{
+   constructor(username,email){
+      super(username)
+      this.email=email
+   }
+}
+
+const iphone= new client("iphone", "iphone@gmail.com")
+// iphone.logMe()
+console.log(iphone.createId());
