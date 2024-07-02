e7d80ff5f4ca3efdb51b7dcc895652b66cd4fda0
AnuradhaLohar <loharanuradha644@gmail.com>
2024-07-02 18:32:29 +0530

    get set method in js done

---
+++ 10_classes_and_oop/10_class_getter_setter.js
@@ -0,0 +1,24 @@
+class user{
+    constructor(email,password){
+        this.email=email,
+        this.password=password
+    }
+
+    get email(){
+        return this._email.toUpperCase();
+    }
+    set email(value){
+         this._email=value
+    }
+
+    get password(){
+        return `${this._password}Anu`
+    }
+    
+    set password(value){
+        this._password=value
+    }
+}
+const result= new user("anu@.com","12345")
+// console.log(result.email);
+console.log(result.password);
